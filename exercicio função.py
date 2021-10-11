4a84b15ace8da7224be2d6f09fe3b9d767cb1424
Azultropico <90474697+Azultropico@users.noreply.github.com>
2021-10-11 19:27:56 -0300

    Add files via upload

---
+++ exercicio função.py
@@ -0,0 +1,28 @@
+def valorPagamento(a,b):
+    multa = (a + 3/100)
+    juros = (a + 0.1/100) * b
+    valorfinal = a + multa + juros
+
+    if b == 0:
+        return a
+    
+    elif b > 0:
+        return valorfinal
+
+lista1 = []
+lista2 = []
+
+while True:
+
+    vparcela = float(input('Digite o valor da parcela: \n'))
+    if vparcela == 0:
+         break
+    dias = int(input('Digite a quantidade de dias de atraso: \n'))
+
+    result = valorPagamento(vparcela, dias)
+    lista1.append(result)
+    lista2.append(dias)
+
+print(f'valor da parcela {lista1}')
+print(f'quantidade de dias de atraso{lista2}')
+
